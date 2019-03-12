5e2f361b4adc16f46635f137634f2eacbc3aa439
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-03-12 07:15:20 -0500

    code complete no logic yet

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace CourseGrader
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            // create a method named GradeTestScores that takes a int array of test scores as its parameter and returns a string
-            // GradeTestScores will add up these test scores and calculate an average score. 
-            // It should then return a string of "pass" or "fail" depending on these two conditions:
-            // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
-            // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+            int[] testScores = new int [] { 80, 50, 95, 75, 40 };
+            Console.WriteLine(SumTestScores(testScores));
+            Console.ReadLine();
         }
+
+
+        public static double  SumTestScores(int [] testScores)
+        {
+            double sum = 0;
+            foreach (int grade in testScores)
+            {
+               sum += grade;
+            }
+            double average = sum / testScores.Length;
+            return average;
+            
+        }
+
+
+
+       
+
     }
+
 }
+         
+ 
